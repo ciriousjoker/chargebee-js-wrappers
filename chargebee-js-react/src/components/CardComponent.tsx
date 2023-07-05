ea747bd9eb426bbfa12b7655f4348aff5698f89f
Philipp Bauer <ciriousjoker@gmail.com>
2023-07-05 17:02:28 +0200

    Move dist folder to top level

--- chargebee-js-react/src/components/CardComponent.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react'
-import ComponentGroup, { ChargebeeComponentProps } from './ComponentGroup'
-
-export default React.forwardRef((props: ChargebeeComponentProps, ref: React.LegacyRef<ComponentGroup>) => (
-    <ComponentGroup type='card' ref={ref} {...props} />
-));
